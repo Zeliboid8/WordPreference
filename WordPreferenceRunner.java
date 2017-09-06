920e10a7a0da95eab29c4621a32ddcb629d97c17
Zeliboid8 <zeliboid8@gmail.com>
2017-09-06 19:51:34 -0400

    Version 1.1
    Finally done in time for the start of the school year.
    New "Show Actions" option will show actions (such as marking or
    searching words) underneath the search results.
    Fixed an issue where marking base words automatically would cause the
    mark button to be unselected after words were searched.

--- WordPreferenceRunner.java
+++ WordPreferenceRunner.java
@@ -1,5 +1,6 @@
 import javafx.application.Application;
 import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -10,6 +11,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckMenuItem;
 import javafx.scene.control.ContextMenu;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Menu;
@@ -42,8 +44,11 @@ public class WordPreferenceRunner extends Application
     private static Set<String> markedWords = new LinkedHashSet<String>(); // Set of marked words
     private static String baseWord;	// Non-conjugated, non-pluralized, etc.
     
+    private static boolean reviewing = false;
+    
     // Menu items
     private static MenuItem translate;
+    private static MenuItem markWord;
     private static MenuItem delete;
     private static MenuItem webpage;
     private static MenuBar menuBar;
@@ -60,6 +65,7 @@ public class WordPreferenceRunner extends Application
     private static CheckMenuItem engToFre;
     private static Menu menuMore;
     private static CheckMenuItem showInfo;
+    private static CheckMenuItem showActions;
     private static CheckMenuItem markBaseWord;
     private static CheckMenuItem markAll;
     private static CheckMenuItem autoSave;
@@ -68,13 +74,19 @@ public class WordPreferenceRunner extends Application
     private static ToggleButton markButton;
     private static Button searchButton;
     
+    // Updating text
+    private static Label actions = new Label();
+    private static SimpleStringProperty text = new SimpleStringProperty();
+    
+    // Settings
     private static boolean enableShowInfo = true;
+    private static boolean enableShowActions = false;
     private static boolean enableMarkBaseWord = true;
     private static boolean enableMarkAll = false;
     private static boolean enableAutoSave = false;
     private static int languageSetting = 1;
     
-    // Potential URL Strings
+    // Potential URL strings
     private static final String url1 = "http://www.wordreference.com/es/en/translation.asp?spen=";	// Spanish to English
     private static final String url2 = "http://www.wordreference.com/es/translation.asp?tranword=";	// English to Spanish
     private static final String url3 = "http://www.wordreference.com/fren/";	// French to English
@@ -112,29 +124,40 @@ public class WordPreferenceRunner extends Application
             translate = new MenuItem();
             translate.textProperty().bind(Bindings.format("Translate", cell.itemProperty()));
             translate.setOnAction(event -> {
-                String item = cell.getItem();
-                wordInput.setText(item);
+            	reviewing = false;
+                String word = cell.getItem();
+                wordInput.setText(word);
                 try {
-					search(item);
+					search(word);
 				} 
                 catch (Exception e1) {
 				}
             });
+            markWord = new MenuItem();
+            markWord.textProperty().bind(Bindings.format("Mark", cell.itemProperty()));
+            markWord.setOnAction(event -> {
+            	if (!reviewing)
+            	{
+            		String word = cell.getItem();
+            		text.set("\"" + word.substring(0, word.lastIndexOf("(") - 2) + "\"" + " added.");
+            		markedWords.add(word.substring(0, word.lastIndexOf("(") - 2));
+            	}
+            	});
             delete = new MenuItem();
             delete.textProperty().bind(Bindings.format("Delete", cell.itemProperty()));
             delete.setOnAction(event -> {
             	String word = cell.getItem();
-            	System.out.println(word + " removed.");
+            	text.set("\"" + word + "\"" + " removed.");
             	markedWords.remove(word);
             	list.getItems().remove(word);
             	});
             webpage = new MenuItem();
             webpage.textProperty().bind(Bindings.format("Webpage", cell.itemProperty()));
             webpage.setOnAction(event -> {
-            	String item = cell.getItem();
+            	String word = cell.getItem();
             	try 
             	{
-					setURL(item);
+					setURL(word);
 				} 
             	catch (Exception e1) {}
             	try 
@@ -143,7 +166,7 @@ public class WordPreferenceRunner extends Application
 				} 
             	catch (Exception e2) {}
             	});
-            contextMenu.getItems().addAll(translate, delete, webpage);
+            contextMenu.getItems().addAll(translate, markWord, delete, webpage);
 
             cell.textProperty().bind(cell.itemProperty());
             cell.emptyProperty().addListener((obs, wasEmpty, isNowEmpty) -> {
@@ -168,6 +191,8 @@ public class WordPreferenceRunner extends Application
         review.setOnAction(new EventHandler<ActionEvent>() {
         	public void handle(ActionEvent click)
         	{
+        		text.set("Reviewing marked words.");
+        		reviewing = true;
         		translations.clear();
         		words.clear();
         		for (String word : markedWords)
@@ -294,7 +319,32 @@ public class WordPreferenceRunner extends Application
         	autoSave.setSelected(true);
         }
         
-        menuMore.getItems().addAll(showInfo, markBaseWord, markAll, autoSave);
+        showActions = new CheckMenuItem("Show Actions");
+        showActions.setOnAction(new EventHandler<ActionEvent>() {
+        	public void handle(ActionEvent click)
+        	{
+        		enableShowActions = !enableShowActions;
+        		if (enableShowActions)
+                {
+                	actions.setVisible(true);
+                }
+        		else
+        		{
+        			actions.setVisible(false);
+        		}
+        	}
+        });
+        if (enableShowActions)
+        {
+        	showActions.setSelected(true);
+        	actions.setVisible(true);
+        }
+        else
+        {
+        	actions.setVisible(false);
+        }
+        
+        menuMore.getItems().addAll(showInfo, showActions, markBaseWord, markAll, autoSave);
         menuBar.getMenus().addAll(menuFile, menuEdit, menuOptions, menuMore);
         
         // Mark/Favorite Button
@@ -318,7 +368,7 @@ public class WordPreferenceRunner extends Application
         			search(wordInput.getText());
 				} catch (Exception e1) 
         		{
-					System.out.println("Word search failed.");
+					text.set("Word search failed.");
 				}
         		if (markedWords.contains(wordInput.getText()))
         		{
@@ -348,7 +398,7 @@ public class WordPreferenceRunner extends Application
             			search(wordInput.getText());
 					} catch (Exception e1) 
             		{
-						System.out.println("Word search failed.");
+						text.set("Word search failed.");
 					}
             	}
             }
@@ -360,13 +410,16 @@ public class WordPreferenceRunner extends Application
         hbox.getChildren().addAll(wordInput, searchButton);
         hbox.setAlignment(Pos.CENTER);
         
+        actions.textProperty().bind(text);
+        actions.managedProperty().bind(actions.visibleProperty());
+        
         Scene scene = new Scene(new VBox(), 420, 290);
         scene.setFill(Color.OLDLACE);
         final VBox vbox = new VBox();
         vbox.setAlignment(Pos.CENTER);
         vbox.setSpacing(10);
         vbox.setPadding(new Insets(10, 10, 10, 10));
-        vbox.getChildren().addAll(hbox, markButton, list);
+        vbox.getChildren().addAll(hbox, markButton, list, actions);
         ((VBox) scene.getRoot()).getChildren().addAll(menuBar, vbox);
         scene.getStylesheets().add("Theme.css");
         window.setScene(scene);
@@ -375,6 +428,7 @@ public class WordPreferenceRunner extends Application
     
     public static void search(String word) throws Exception 
     {
+    	reviewing = false;
     	translations.clear();
     	boolean foundBaseWord = false;
     	while (word.contains(" "))
@@ -393,9 +447,10 @@ public class WordPreferenceRunner extends Application
         			String beginning = inputLine.substring(inputLine.indexOf("class='ToWrd'") + 15);
         			String end = beginning.substring(0, beginning.indexOf("<"));
         			String finalWord = convert(end);
-        			String beginningCutGender = beginning.substring(beginning.indexOf("POS2'") + 6);
-        			String endCutGender = beginningCutGender.substring(0, beginningCutGender.indexOf("<"));
-        			translations.put(finalWord, endCutGender);
+        			String beginningCutInfo = beginning.substring(beginning.indexOf("POS2'") + 6);
+        			String endCutInfo = beginningCutInfo.substring(0, beginningCutInfo.indexOf("<"));
+        			String finalInfo = convert(endCutInfo);
+        			translations.put(finalWord, finalInfo);
         		}
         	}
         	if (!foundBaseWord && inputLine.contains("<td class='FrWrd' ><strong>"))
@@ -406,30 +461,42 @@ public class WordPreferenceRunner extends Application
         		foundBaseWord = true;
         	}
         }
-        if (enableMarkAll)
+        if (!enableMarkAll)
+        {
+	        if (markedWords.contains(wordInput.getText()))
+			{
+				markButton.setSelected(true);
+			}
+			else
+			{
+				markButton.setSelected(false);
+			}
+        }
+        else
     	{
         	if (translations.size() != 0)
         	{
-        		markedWords.add(word);
         		markButton.setSelected(true);
         		if (enableMarkBaseWord)
         		{
-        			System.out.println(baseWord + " added.");
+        			markedWords.add(convert(baseWord));
+        			text.set("\"" + baseWord + "\"" + " added.");
         		}
         		else
         		{
-        			System.out.println(word + " added.");
+        			markedWords.add(convert(word));
+        			text.set("\"" + word + "\"" + " added.");
         		}
         	}
     	}
-        if (markedWords.contains(wordInput.getText()))
-		{
-			markButton.setSelected(true);
-		}
-		else
-		{
-			markButton.setSelected(false);
-		}
+        if (translations.size() == 0)
+        {
+        	text.set("No translations found.");
+        }
+        else
+        {
+        	text.set("Showing translations for \"" + word + ".\"");
+        }
         update();
         in.close();
     }
@@ -492,12 +559,12 @@ public class WordPreferenceRunner extends Application
 	    		if (enableMarkBaseWord)
 	    		{
 	    			markedWords.remove(baseWord);
-	    			System.out.println(baseWord + " removed.");
+	    			text.set("\"" + baseWord + "\"" + " removed.");
 	    		}
 	    		else
 	    		{
 	    			markedWords.remove(word);
-	    			System.out.println(word + " removed.");
+	    			text.set("\"" + word + "\"" + " removed.");
 	    		}
 	    		markButton.setSelected(false);
 	    	}
@@ -505,13 +572,13 @@ public class WordPreferenceRunner extends Application
 	    	{
 	    		if (enableMarkBaseWord)
 	    		{
-	    			markedWords.add(baseWord);
-	    			System.out.println(baseWord + " added.");
+	    			markedWords.add(convert(baseWord));
+	    			text.set("\"" + baseWord + "\"" + " added.");
 	    		}
 	    		else
 	    		{
-	    			markedWords.add(word);
-	    			System.out.println(word + " added.");
+	    			markedWords.add(convert(word));
+	    			text.set("\"" + word + "\"" + " added.");
 	    		}
 	    		markButton.setSelected(true);
 	    	}
@@ -529,11 +596,11 @@ public class WordPreferenceRunner extends Application
         {
         	if (enableShowInfo && e.getValue().length() != 0)
         	{
-        		words.add(e.getKey() + " (" + e.getValue() + ")");
+        		words.add(convert(e.getKey()) + " (" + convert(e.getValue() + ")"));
         	}
         	else
         	{
-        		words.add(e.getKey());
+        		words.add(convert(e.getKey()));
         	}
         }
     	list.setItems(words);
@@ -563,17 +630,18 @@ public class WordPreferenceRunner extends Application
     		Formatter file = new Formatter("settings.txt");
     		file.format("%s" + System.getProperty("line.separator"), languageSetting);		// Remembering the language setting
     		file.format("%s" + System.getProperty("line.separator"), enableShowInfo);		// Remembering whether extra information should be shown
+    		file.format("%s" + System.getProperty("line.separator"), enableShowActions);	// Remembering whether actions should be shown
     		file.format("%s" + System.getProperty("line.separator"), enableMarkBaseWord);	// Remembering mark base word settings
     		file.format("%s" + System.getProperty("line.separator"), enableMarkAll);		// Remembering mark all settings
     		file.format("%s" + System.getProperty("line.separator"), enableAutoSave);		// Remembering auto-save settings
     		while (itr.hasNext())
     		{
-    			file.format("%s\n", itr.next());
+    			file.format("%s" + System.getProperty("line.separator"), itr.next());
     		}
     		file.close();
     	}
     	catch(Exception e) {}
-    	System.out.println("File saved.");
+    	text.set("File saved.");
     }
     
     public static void loadFile()
@@ -586,6 +654,7 @@ public class WordPreferenceRunner extends Application
 	    	{
 	    		languageSetting = Integer.parseInt(sc.nextLine());		// Setting language setting
 	    		enableShowInfo = Boolean.valueOf(sc.nextLine());		// Setting extra information setting
+	    		enableShowActions = Boolean.valueOf(sc.nextLine());		// Setting show actions setting
 	    		enableMarkBaseWord = Boolean.valueOf(sc.nextLine());	// Setting mark base word setting
 	    		enableMarkAll = Boolean.valueOf(sc.nextLine());			// Setting mark all setting
 	    		enableAutoSave = Boolean.valueOf(sc.nextLine());		// Setting auto-save setting
@@ -599,7 +668,7 @@ public class WordPreferenceRunner extends Application
     	catch(FileNotFoundException exception)
     	{
     	}
-    	System.out.println("File loaded.");
+    	text.set("File loaded.");
     }
     
     public static void updateInstructions()
